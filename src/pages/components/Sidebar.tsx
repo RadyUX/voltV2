45d6c0b793afe8142ae4b4457bca087948e40051
Rafaele <rafaele.sinaguglia@gmail.com>
2023-08-21 13:26:22 +0200

    usestate error fixed

--- src/pages/components/Sidebar.tsx
+++ src/pages/components/Sidebar.tsx
@@ -44,7 +44,7 @@ export default function Sidebar() {
                     </button>
                 </form>
                 <ul className="space-y-4">
-                    {collections.data.map((collection) => (
+                    {collections?.data?.map((collection) => (
                       <Link href={`http://localhost:3000/${collection.id}`}>
                         <li key={collection.id} className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow hover:shadow-md transform hover:scale-105 transition-transform duration-150 ease-in-out border dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-900">
                             <p className="text-gray-800 dark:text-gray-100 font-medium">{collection.name}</p>
